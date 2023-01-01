8c4b249c31612088024c2f168fab3863d2286fb4
Diogo Biazus <diogo@biazus.ca>
2023-01-01 14:56:41 -0500

    Remove big MaybeT and convert all Nothing values to Exceptions in IO

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,26 +10,19 @@ import Control.Monad.Catch (throwM)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
 import Data.Foldable (find)
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, fromJust)
 import Data.Text (Text)
 import qualified GI.Gst as GST
 import qualified GI.Gst.Objects.Element as GST
 
-data GSError = LinkingError | StateChangeError | WaitingError
+data GSError = LinkingError | StateChangeError | WaitingError | AddError | FactoryError | BusError | MessageError | PadError
   deriving (Show)
 
 instance Exception GSError
 
 play :: Config -> IO ()
 play config = do
-  p <- runMaybeT $ maybePlay config
-  case p of
-    Nothing -> error "Something when wrong"
-    Just msg -> print msg
-
-maybePlay :: Config -> MaybeT IO ()
-maybePlay config = do
-  pipeline <- makePipeline
+  pipeline <-  makePipeline
   elements <- addMany pipeline [if configTest config then "videotestsrc" else "libcamerasrc", "videoconvert", "motioncells", "videoconvert", "tee", "queue", "autovideosink", "queue", "vp9enc", "rtpvp9pay", "udpsink"]
   let source : _ : _ : lastConvert : tee : _ : _ : queue : vp9 : rtp : udpSink : _ = elements
       pairsToLink = (zip <*> tail) (take 7 elements)
@@ -38,8 +31,8 @@ maybePlay config = do
   linkResults <- mapM (uncurry GST.elementLink) pairsToLink
   unless (and linkResults) (throwM LinkingError)
 
-  teeVideoSinkPad <- MaybeT $ GST.elementGetRequestPad tee "src_%u"
-  qVideoSinkPad <- MaybeT $ GST.elementGetStaticPad queue "sink"
+  teeVideoSinkPad <- maybeThrow PadError $ GST.elementGetRequestPad tee "src_%u"
+  qVideoSinkPad <- maybeThrow PadError $ GST.elementGetStaticPad queue "sink"
   r <- GST.padLink teeVideoSinkPad qVideoSinkPad
 
   unless (r == GST.PadLinkReturnOk) (throwM LinkingError)
@@ -51,14 +44,14 @@ maybePlay config = do
   unless (and linkResults) (throwM LinkingError)
 
   -- recording portion of the pipeline
-  teePad <- MaybeT $ GST.elementGetRequestPad tee "src_%u"
+  teePad <- maybeThrow PadError $ GST.elementGetRequestPad tee "src_%u"
   [queue, valve, vp9enc, webmmux, filesink] <- addMany pipeline ["queue", "valve", "vp9enc", "webmmux", "filesink"]
   GST.utilSetObjectArg filesink "location" "./test.webm"
   GST.utilSetObjectArg valve "drop" "true"
   linkResults <- mapM (uncurry GST.elementLink) [(queue, vp9enc), (vp9enc, webmmux), (webmmux, filesink)]
   unless (and linkResults) (throwM LinkingError)
 
-  queuePad <- MaybeT $ GST.elementGetStaticPad queue "sink"
+  queuePad <- maybeThrow PadError $ GST.elementGetStaticPad queue "sink"
   r <- GST.padLink teePad queuePad
   unless (r == GST.PadLinkReturnOk) (throwM LinkingError)
 
@@ -89,7 +82,7 @@ maybePlay config = do
           liftIO $ print errorMessage
           throwM WaitingError
         [GST.MessageTypeElement] -> do
-          str <- MaybeT $ GST.messageGetStructure msg
+          str <- maybeThrow MessageError $ GST.messageGetStructure msg
           nfields <- GST.structureNFields str
           when (nfields > 0) $ do
             fname <- GST.structureNthFieldName str (fromIntegral nfields - 1)
@@ -100,24 +93,31 @@ maybePlay config = do
 
       respondToMessages pipeline valve
 
-    makePipeline :: MaybeT IO GST.Pipeline
+    maybeThrow :: GSError -> IO (Maybe a)-> IO a
+    maybeThrow error action = do
+      maybeAction <- action
+      case maybeAction of
+        Just a -> pure a
+        Nothing -> throwM error
+
+    makePipeline :: IO GST.Pipeline
     makePipeline = GST.init Nothing >> GST.pipelineNew Nothing
 
-    waitForMessageTypes :: [GST.MessageType] -> GST.Pipeline -> MaybeT IO GST.Message
+    waitForMessageTypes :: [GST.MessageType] -> GST.Pipeline -> IO GST.Message
     waitForMessageTypes messageTypes pipeline = do
-      b <- MaybeT $ GST.elementGetBus pipeline
-      MaybeT $ GST.busTimedPopFiltered b GST.CLOCK_TIME_NONE messageTypes
+      b <- maybeThrow BusError $ GST.elementGetBus pipeline
+      maybeThrow MessageError $ GST.busTimedPopFiltered b GST.CLOCK_TIME_NONE messageTypes
 
-    waitForErrorOrEosOrElement :: GST.Pipeline -> MaybeT IO GST.Message
+    waitForErrorOrEosOrElement :: GST.Pipeline -> IO GST.Message
     waitForErrorOrEosOrElement = waitForMessageTypes [GST.MessageTypeError, GST.MessageTypeEos, GST.MessageTypeElement]
 
-    addMany :: GST.Pipeline -> [Text] -> MaybeT IO [GST.Element]
+    addMany :: GST.Pipeline -> [Text] -> IO [GST.Element]
     addMany pipeline names = do
-      elements <- mapM make names
+      let make = flip GST.elementFactoryMake Nothing
+      let maybeMake name = maybeThrow FactoryError $ make name
+      elements <- mapM maybeMake names
       addResults <- mapM (GST.binAdd pipeline) elements
-      when (any not addResults) (error "Could not add some element")
+      when (any not addResults) (throwM AddError)
       pure elements
-
-    make kind = MaybeT $ GST.elementFactoryMake kind Nothing
 
 -- ❯ gst-launch-1.0 -v udpsrc port=5000  ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)VP9, payload=(int)96, ssrc=(uint)101494402, timestamp-offset=(uint)1062469180, seqnum-offset=(uint)10285, a-framerate=(string)30" ! rtpvp9depay ! vp9dec ! decodebin ! videoconvert ! autovideosink sync=false
